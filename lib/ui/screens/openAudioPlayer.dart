282454a6b069b74f9a9c42d2cb46c82ebcc2bf7b
quadroce <francesco_mucci@hotmail.com>
2023-03-30 00:28:17 +0200

    migliorata grafica

--- lib/ui/screens/openAudioPlayer.dart
+++ lib/ui/screens/openAudioPlayer.dart
@@ -2,6 +2,7 @@ import 'package:share/share.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:just_audio/just_audio.dart';
+import 'topbar.dart';
 
 class trasmissione {
   String title;
@@ -88,31 +89,38 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
   @override
   Widget build(BuildContext context) {
     Duration? _duration = _audioPlayer.duration ?? Duration.zero;
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+    final GlobalKey<ScaffoldState> _bottomKey = GlobalKey<ScaffoldState>();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.mediumArticleNome),
+      appBar: RadioAppBar(
+        scaffoldKey: _scaffoldKey,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-              child: Row(
+      body: OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: orientation == Orientation.portrait
+                ? MainAxisAlignment.center
+                : MainAxisAlignment.start,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   FadeInImage.assetNetwork(
                     placeholder: 'assets/images/placeholder.png',
                     image: widget.itunesImage.toString(),
-                    fit: BoxFit.contain,
+                    fit: BoxFit.cover,
+                    width: orientation == Orientation.portrait
+                        ? MediaQuery.of(context).size.width * 0.8
+                        : MediaQuery.of(context).size.width * 0.4,
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 16), // add some spacing between the rows
 
-            Flexible(
-              child: Row(
+              SizedBox(height: 16), // add some spacing between the rows
+
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(widget.mediumArticletitle,
@@ -122,113 +130,132 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
                       )),
                 ],
               ),
-            ),
-            SizedBox(height: 16), // add some spacing between the rows
-            Flexible(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.forward_30),
-                    onPressed: () async {
-                      Duration newPosition =
-                          _audioPlayer.position + Duration(seconds: 30);
-                      await _audioPlayer.seek(newPosition);
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.pause),
-                    onPressed: () async {
-                      await _audioPlayer.pause();
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.play_arrow,
+
+              SizedBox(height: 16), // add some spacing between the rows
+              Flexible(
+                child: Row(
+                  mainAxisAlignment: orientation == Orientation.portrait
+                      ? MainAxisAlignment.center
+                      : MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.forward_30),
+                      onPressed: () async {
+                        Duration newPosition =
+                            _audioPlayer.position + Duration(seconds: 30);
+                        await _audioPlayer.seek(newPosition);
+                      },
                     ),
-                    onPressed: () async {
-                      await _audioPlayer.play();
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.replay_30),
-                    onPressed: () async {
-                      Duration newPosition =
-                          _audioPlayer.position + Duration(seconds: -30);
-                      await _audioPlayer.seek(newPosition);
-                    },
-                  ),
-                ],
+                    IconButton(
+                      icon: Icon(Icons.pause),
+                      onPressed: () async {
+                        await _audioPlayer.pause();
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.play_arrow,
+                      ),
+                      onPressed: () async {
+                        await _audioPlayer.play();
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.replay_30),
+                      onPressed: () async {
+                        Duration newPosition =
+                            _audioPlayer.position + Duration(seconds: -30);
+                        await _audioPlayer.seek(newPosition);
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            StreamBuilder<Duration>(
-              stream: _audioPlayer.positionStream,
-              builder:
-                  (BuildContext context, AsyncSnapshot<Duration> snapshot) {
-                final Duration position = snapshot.data ?? Duration.zero;
-                final String positionText =
-                    position.toString().split('.').first;
-                final String durationText =
-                    _duration.toString().split('.').first;
-                return StatefulBuilder(
-                  builder: (BuildContext context, StateSetter setState) {
-                    _duration = _audioPlayer.duration;
-                    setState(() {}); // Force a rebuild of the widget
-                    return Column(
-                      children: [
-                        Slider(
-                          value: position.inSeconds.toDouble(),
-                          max: _duration?.inSeconds.toDouble() ?? 0.0,
-                          onChanged: (double value) {
-                            setState(() {
-                              _audioPlayer
-                                  .seek(Duration(seconds: value.toInt()));
-                            });
-                          },
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              '$positionText/$durationText',
-                              style: TextStyle(
-                                fontSize: 18,
+              StreamBuilder<Duration>(
+                stream: _audioPlayer.positionStream,
+                builder:
+                    (BuildContext context, AsyncSnapshot<Duration> snapshot) {
+                  final Duration position = snapshot.data ?? Duration.zero;
+                  final String positionText =
+                      position.toString().split('.').first;
+                  final String durationText =
+                      _duration.toString().split('.').first;
+
+                  final Duration remaining =
+                      _duration != null ? _duration! - position : Duration.zero;
+
+                  final String remainingText =
+                      '${remaining.toString().split('.').first}';
+                  return StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState) {
+                      _duration = _audioPlayer.duration;
+                      setState(() {}); // Force a rebuild of the widget
+                      return Column(
+                        children: [
+                          Slider(
+                            value: position.inSeconds.toDouble(),
+                            max: _duration?.inSeconds.toDouble() ?? 0.0,
+                            onChanged: (double value) {
+                              setState(() {
+                                _audioPlayer
+                                    .seek(Duration(seconds: value.toInt()));
+                              });
+                            },
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 16.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '$positionText',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  '$remainingText',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                              height: 16), // add some spacing between the rows
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.share),
+                                onPressed: () {
+                                  final String testoDaCondividere =
+                                      "Ascolta la trasmissione: " +
+                                          widget.mediumArticletitle +
+                                          " su " +
+                                          widget.mediumArticleLink +
+                                          "\n" +
+                                          "Segui \#radio20158 sui social";
+                                  Share.share(testoDaCondividere);
+                                },
                               ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            height: 16), // add some spacing between the rows
-
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.share),
-                              onPressed: () {
-                                final String testoDaCondividere =
-                                    "Ascolta la trasmissione: " +
-                                        widget.mediumArticletitle +
-                                        " su " +
-                                        widget.mediumArticleLink +
-                                        "\n" +
-                                        "Segui \#radio20158 sui social";
-                                Share.share(testoDaCondividere);
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomBarAppBar(key: _bottomKey),
     );
   }
 }
